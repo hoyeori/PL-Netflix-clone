05182fe231ea92241d04be36e05d3b3643d99213
Hoyeol Choi <ichy1004@naver.com>
2022-03-22 19:53:56 +0900

    rebuild

---
+++ src/pages/DetailPage/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from '../../api/axios';
+
+const DetailPage = () => {
+
+    const { movieId } = useParams();
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(`/movie/${movieId}`);
+            setMovie(request.data);
+        }
+        fetchData();
+    }, [movieId]);
+
+    if (!movie) return <div>...loading</div>;
+
+    console.log(movie);
+
+    return (
+        <section>
+            <img
+                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                alt="poster"
+                className="modal__poster-img"
+            />
+        </section>
+    )
+}
+
+export default DetailPage
+
